cf943bce11074c7a53423fe6a614227e5c0e40e8
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-11-29 19:34:49 -0600

    Fixed octicons typing

--- BeatMods2-Frontend/src/components/Octicon.vue
+++ BeatMods2-Frontend/src/components/Octicon.vue
@@ -13,7 +13,6 @@
 </template>
 
 <script lang="ts">
-import "./octicons";
 import Octicons from "@primer/octicons";
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
--- BeatMods2-Frontend/src/components/octicons.ts
+++
@@ -1,1 +0,0 @@
-// this is needed because otherwise the compiler complains
