05c86976dac430ba5f535f16d386dbe81fde022f
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-12-17 20:24:45 -0600

    Added some frontent types for the API responses

---
+++ BeatMods2-Frontend/src/api/index.ts
@@ -0,0 +1,3 @@
+export declare interface IApiRoutes {
+    login: string; // the route to request login information
+}
---
+++ BeatMods2-Frontend/src/api/login.ts
@@ -0,0 +1,3 @@
+export declare interface ILoginResponse {
+    authTarget: string; // the full URL to the OAuth request for authentication
+}
